6a15ca7f8ff7050dba524c9bb62ec3e21e83777c
AyiengaJoan <ayie0002@algonquinlive.com>
2025-01-13 12:25:47 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Task 1
+let a = 3;
+let b = 5;
+let c;
+
+
+let output = 
+    `
+        let a = 3;
+        let b = 5;
+        let c;
+
+        --------------
+
+        a + b = ${a + b}
+        a - b = ${a - b}
+        a * b = ${a * b}
+        a / b = ${a / b}
+        a % b = ${a % b}
+        a += b = ${a += b}
+        a -= b = ${a -= b}
+        a *= b = ${a *= b}
+        a /= b = ${a /= b}
+        a %= b = ${a %= b}
+        a == b = ${a == b}
+        a != b = ${a != b}
+        a > b = ${a > b}
+        a < b = ${a < b}
+        !a && !c = ${!a && !c}
+        !a || !c = ${!a || !c}
+    `
+
+
+console.log(output);
+
+// Task 2
+
+let first_name = "Joan";
+let last_name = "Ayienga";
+let email = "ayie0002@algonquinlive.com";
+let output2;
+
+output2 = 
+`
+    My name is ${first_name} ${last_name}. You can contact me at ${email}.
+`;
+console.log(output2);
